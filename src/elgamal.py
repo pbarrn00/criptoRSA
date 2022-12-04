82b74ed8084763deaf8a0cf2170c85cfbdeb9699
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2022-12-04 02:18:33 +0100

    el gamal formato correcto, funciona

--- src/elgamal.py
+++ src/elgamal.py
@@ -1,7 +1,7 @@
 #ElGamal Implementation
 import random
 import secrets
-from funcs import (blocks_from_bytes, bytes_from_block, compute_block_size, power_mod, multiplicative_inverse)
+from funcs import (block_from_bytes, blocks_from_bytes, bytes_from_block, compute_block_size, power_mod, multiplicative_inverse)
 from diffie_hellman import (diffie_primes)
 
 def generate_public_key(p: int, g: int, ai: int) -> int:
@@ -40,7 +40,7 @@ def elgamal_keygen(p: int, g: int) -> tuple:
     my_pk = generate_public_key(p, g, ai)
     return my_pk, ai
 
-def elgamal_encrypt(by: bytes, g: int, k: int, pk_bob: int, p: int) -> tuple[int, bytes]:
+def elgamal_encrypt(by: bytes, g: int, k: int, pk_bob: int, p: int) -> list[tuple[int, bytes]]:
     '''
     Encrypts a message using ElGamal
     Parameters
@@ -70,11 +70,10 @@ def elgamal_encrypt(by: bytes, g: int, k: int, pk_bob: int, p: int) -> tuple[int
     #print("Encrypted block: "+str(encrypted))
     encryptedC1 = []
     encryptedC2 = []
-        
-    encryptedC1 = [block[0].to_bytes(encrypted_block_size, byteorder="big") 
-                for block in encrypted]
-    encryptedC2 = [block[1].to_bytes(encrypted_block_size, byteorder="big") 
-                for block in encrypted]
+    
+    for block in encrypted:
+        encryptedC1.append(block[0])
+        encryptedC2.append(block[1].to_bytes(encrypted_block_size, 'big'))
 
     #print("encrypted C1: "+str(encryptedC1))
     #print("encrypted C2: "+str(encryptedC2))
@@ -86,17 +85,19 @@ def elgamal_encrypt(by: bytes, g: int, k: int, pk_bob: int, p: int) -> tuple[int
     # This is necessary to properly decrypt leading null bytes
     padding_block = elgamal_encryption(
         last_size.to_bytes(block_size, byteorder="big"), g, pk_bob, p, block_size)
-    padding_blockC1 = [block[0].to_bytes(encrypted_block_size, byteorder="big")
-                     for block in padding_block]
-    padding_blockC2 = [block[1].to_bytes(encrypted_block_size, byteorder="big")
-                     for block in padding_block]
+    
+    for block in padding_block:
+        encryptedC1.append(block[0])
+        encryptedC2.append(block[1].to_bytes(encrypted_block_size, 'big'))
 
     #print("padding_block C1: "+str(padding_blockC1))
     #print("padding_block C2: "+str(padding_blockC2))
 
-    encryptedC1 = (b'').join(encryptedC1 + padding_blockC1)
-    encryptedC2 = (b'').join(encryptedC2 + padding_blockC2)
-    return (encryptedC1, encryptedC2)
+    list = []
+    for elementC1, elementC2 in zip(encryptedC1, encryptedC2):
+        list.append((elementC1, elementC2))
+    
+    return list
 
 def elgamal_encryption(by: bytes, g: int, pk_bob: int, p: int, extract_blocks_size: int
                    ) -> list[tuple[int, int]]:
@@ -119,12 +120,12 @@ def elgamal_encryption(by: bytes, g: int, pk_bob: int, p: int, extract_blocks_si
     list[tuple[int, int]]
         Encrypted message
     '''
-    key = secrets.choice(range(1, p-1))
 
     blocks = blocks_from_bytes(by, extract_blocks_size)
     encryptions = []
 
     for block in blocks:
+        key = secrets.choice(range(1, p-1))
         C1 = power_mod(g, key, p)
         C2 = (block*power_mod(pk_bob, key, p))%p
         encryptions.append((C1, C2))
@@ -147,32 +148,59 @@ def elgamal_decrypt(by: bytes, p: int, ai: int) -> bytes:
     int
         Decrypted message
     '''
-    #print("Bytes: "+str(by))
+    decryptedC1 = []
+    decryptedC2 = []
+    for block in by:
+        decryptedC1.append(block[0])
+        decryptedC2.append(block[1])
+    
+    print("Decrypted C1: "+str(decryptedC1))
+    print("Decrypted C2: "+str(decryptedC2))
+
     encrypted_block_size = compute_block_size(p) + 1
 
-    decrypted = elgamal_decryption(by, ai, p, encrypted_block_size)
-    #print("Decrypted: "+str(decrypted))
+    decrypted = elgamal_decryption(decryptedC1, decryptedC2, ai, p)
     last_size = decrypted[-1]
     
     # decrypt the last block independently
     last_block = [bytes_from_block(decrypted[-2], last_size)]
     
+    
     decrypted = decrypted[:-2]
+
     decrypted = [bytes_from_block(block, encrypted_block_size - 1) 
                  for block in decrypted]
-    #print("Decrypted bytes: "+str(decrypted))
+    print("Decrypted bytes: "+str(decrypted))
     decrypted = (b'').join(decrypted + last_block)
-    
     return decrypted
 
-def elgamal_decryption(by, pk: int, p: int, extract_blocks_size: int
+def elgamal_decryption(listC1: list, listC2: list, ai: int, p: int
                    ) -> list[int]:
+    '''
+    Decrypts a message using ElGamal
+    Parameters
+    ----------
+    listC1 : list
+        List of C1
+    listC2 : list
+        List of C2
+    ai : int
+        Private key
+    p : int 
+        Prime number
+    Returns 
+    -------     
+    list[int]
+        Decrypted message
+    '''
+    blocksC1 = []
+    blocksC2 = []
 
-    blocksC1 = blocks_from_bytes(by[0], extract_blocks_size)
-    blocksC2 = blocks_from_bytes(by[1], extract_blocks_size)
-    C1 = blocksC1[0]
+    for elementC1, elementC2 in zip(listC1, listC2):
+        blocksC1.append(elementC1)
+        blocksC2.append(block_from_bytes(elementC2))        
 
-    return [block*multiplicative_inverse(power_mod(C1, pk, p), p)%p for block in blocksC2]
+    return [blockC2*multiplicative_inverse(power_mod(blockC1, ai, p), p)%p for blockC1, blockC2 in zip(blocksC1, blocksC2)]
 
 def main():
     # Generate p, g and public key
@@ -204,6 +232,7 @@ def main():
     # Encrypt and decrypt a message
     encrypted = elgamal_encrypt(my_message_encoded, g, k, pk_bob, p)
     print("Encrypted: "+str(encrypted))
+
     decrypted = elgamal_decrypt(encrypted, p, ai)
     message = decrypted.decode('utf-8')
     print("Decrypted: "+str(message))
